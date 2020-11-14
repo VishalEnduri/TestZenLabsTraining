ca0975056b7b96826beccd50cb35abefe34a7161
LAPTOP-OSS12VPA\User <e4vishal@gmail.com>
2020-11-14 16:40:17 +0000

    First day Session on Traning

---
+++ NonStaticFunWithoutReturnType/Demo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonStaticFunWithoutReturnType
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            Sample s = new Sample();
+            s.fun();
+        }
+
+    }
+}
---
+++ NonStaticFunWithoutReturnType/Sample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonStaticFunWithoutReturnType
+{
+    public class Sample
+    {
+        public string s = "vishal";
+        public void fun()
+        {
+            Console.WriteLine("Name of the student is " + s);
+        }
+                
+    }
+}
